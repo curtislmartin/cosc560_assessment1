25a53781f6eecdf08a64f66020585988f799e501
Curtis Martin <curtismartin2008@gmail.com>
2026-06-17 23:50:09 +1000

    define route stubs for guideline 3

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+// mostly stubs
+
+Route::get('/all', function(){
+    return view('posts');
+});
+
+Route::get('/create', function(){
+    return view('create');
+});
+
+Route::get('/edit/{id}',[App\Http\Controllers\PostController::class,'editPost']); // not yet built
+
+Route::post('/save');
+
+Route::get('/delete/{id}');
